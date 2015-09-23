467ad951af19f6813303f22e9ddddb8984eae8d4
Vladimir Mikhailov <vmikhailov@callfire.com>
2015-09-23 13:58:01 +0300

    added dnc api test

--- src/test/java/com/callfire/api/client/api/integration/contacts/DncApiTest.java
+++ src/test/java/com/callfire/api/client/api/integration/contacts/DncApiTest.java
@@ -1,30 +1,39 @@
 package com.callfire.api.client.api.integration.contacts;
 
-import com.callfire.api.client.api.contacts.model.Contact;
-import com.callfire.api.client.api.contacts.model.request.AddContactListItemsRequest;
-import com.callfire.api.client.api.contacts.model.request.CreateContactListRequest;
+import com.callfire.api.client.CallfireClient;
+import com.callfire.api.client.api.common.model.Page;
+import com.callfire.api.client.api.contacts.model.DoNotContact;
+import com.callfire.api.client.api.contacts.model.request.FindDncContactsRequest;
 import com.callfire.api.client.api.integration.AbstractIntegrationTest;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 /**
- * integration tests for /contacts/lists api endpoint
+ * integration tests for /contacts/do-not-calls api endpoint
  */
 public class DncApiTest extends AbstractIntegrationTest {
 
     @Test
-    public void testAddContactList() throws Exception {
-
-        AddContactListItemsRequest<Contact> request = AddContactListItemsRequest.<Contact>create()
-            .contactNumbersField("")
-            .contacts(Arrays.asList(new Contact(), new Contact()))
+    public void testFindDncContacts() throws Exception {
+        FindDncContactsRequest request = FindDncContactsRequest.create()
+            .textDnc(true)
+            .limit(1L)
             .build();
+        CallfireClient client = getCallfireClient();
+        Page<DoNotContact> dncContacts = client.getContactsApi().getDncApi().findDncContacts(request);
+        Assert.assertEquals(1, dncContacts.getItems().size());
 
-        CreateContactListRequest.<Long>create()
-            .name("name")
-            .contacts(Arrays.asList(1L, 2L))
-            .build();
+        System.out.println(dncContacts);
+    }
 
+    @Test
+    public void testUpdateDncNumber() throws Exception {
+        CallfireClient client = getCallfireClient();
+        DoNotContact number = new DoNotContact();
+        number.setListId(1700040003L);
+        number.setNumber("16505541938");
+        number.setCall(true);
+        number.setText(true);
+        client.getContactsApi().getDncApi().updateDncNumber(number);
     }
 }
