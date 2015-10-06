468a880cd5d163c5403759ad251d633f35251304
vladimir-mhl <vladimir.mhl@gmail.com>
2015-10-06 12:08:28 +0300

    added tests for voice broadcasts

--- src/test/java/com/callfire/api/client/api/campaigns/VoiceBroadcastsApiTest.java
+++ src/test/java/com/callfire/api/client/api/campaigns/VoiceBroadcastsApiTest.java
@@ -3,15 +3,19 @@ package com.callfire.api.client.api.campaigns;
 import com.callfire.api.client.api.AbstractApiTest;
 import com.callfire.api.client.api.callstexts.model.Call;
 import com.callfire.api.client.api.campaigns.model.Batch;
-import com.callfire.api.client.api.campaigns.model.IvrBroadcast;
 import com.callfire.api.client.api.campaigns.model.Recipient;
+import com.callfire.api.client.api.campaigns.model.VoiceBroadcast;
+import com.callfire.api.client.api.campaigns.model.VoiceBroadcast.AnsweringMachineConfig;
 import com.callfire.api.client.api.campaigns.model.request.AddBatchRequest;
-import com.callfire.api.client.api.campaigns.model.request.FindIvrBroadcastsRequest;
+import com.callfire.api.client.api.campaigns.model.request.FindVoiceBroadcastsRequest;
 import com.callfire.api.client.api.common.model.ListHolder;
 import com.callfire.api.client.api.common.model.Page;
 import com.callfire.api.client.api.common.model.ResourceId;
 import com.callfire.api.client.api.common.model.request.GetByIdRequest;
-import org.apache.http.client.methods.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -22,46 +26,45 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-public class IvrBroadcastsApiTest extends AbstractApiTest {
-    private static final String JSON_PATH = BASE_PATH + "/campaigns/ivrBroadcastsApi";
+public class VoiceBroadcastsApiTest extends AbstractApiTest {
+    private static final String JSON_PATH = BASE_PATH + "/campaigns/voiceBroadcastsApi";
 
     @Test
     public void testFind() throws Exception {
-        String expectedJson = getJsonPayload(JSON_PATH + "/response/findIvrBroadcasts.json");
+        String expectedJson = getJsonPayload(JSON_PATH + "/response/findVoiceBroadcasts.json");
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse, expectedJson);
 
-        FindIvrBroadcastsRequest request = FindIvrBroadcastsRequest.create()
+        FindVoiceBroadcastsRequest request = FindVoiceBroadcastsRequest.create()
             .limit(5L)
-            .offset(0L)
             .name("name")
-            .inbound(true)
+            .label("label")
+            .running(true)
             .build();
-        Page<IvrBroadcast> broadcasts = client.ivrBroadcastsApi().find(request);
+        Page<VoiceBroadcast> broadcasts = client.voiceBroadcastsApi().find(request);
         assertThat(jsonConverter.serialize(broadcasts), equalToIgnoringWhiteSpace(expectedJson));
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpGet.METHOD_NAME, arg.getMethod());
         assertNull(extractHttpEntity(arg));
-        assertUriContainsQueryParams(arg.getURI(), "limit=5", "offset=0", "name=name", "inbound=true");
+        assertUriContainsQueryParams(arg.getURI(), "limit=5", "label=label", "name=name", "running=true");
     }
 
     @Test
     public void testCreate() throws Exception {
-        String responseJson = getJsonPayload(JSON_PATH + "/response/createIvrBroadcast.json");
-        String requestJson = getJsonPayload(JSON_PATH + "/request/createIvrBroadcast.json");
+        String responseJson = getJsonPayload(JSON_PATH + "/response/createVoiceBroadcast.json");
+        String requestJson = getJsonPayload(JSON_PATH + "/request/createVoiceBroadcast.json");
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse, responseJson);
 
-        IvrBroadcast ivrBroadcast = new IvrBroadcast();
-        ivrBroadcast.setName("Example API IVR");
-        ivrBroadcast.setFromNumber("12135551189");
+        VoiceBroadcast voiceBroadcast = new VoiceBroadcast();
+        voiceBroadcast.setName("Example API VB");
+        voiceBroadcast.setFromNumber("12135551189");
+        voiceBroadcast.setAnsweringMachineConfig(AnsweringMachineConfig.AM_AND_LIVE);
+        voiceBroadcast.setLiveSoundText("Hello! This is a live answer text to speech recording");
+        voiceBroadcast.setMachineSoundText("This is an answering machine text to speech recording");
         Recipient r1 = new Recipient();
-        r1.setPhoneNumber("12135551100");
-        Recipient r2 = new Recipient();
-        r2.setPhoneNumber("12135551101");
-        ivrBroadcast.setRecipients(asList(r1, r2));
-        ivrBroadcast.setDialplanXml(
-            "<dialplan name=\"Root\"><play type=\"tts\">Congratulations! You have successfully configured a CallFire I V R.</play></dialplan>");
-        ResourceId id = client.ivrBroadcastsApi().create(ivrBroadcast, true);
+        r1.setPhoneNumber("2135551133");
+        voiceBroadcast.setRecipients(Collections.singletonList(r1));
+        ResourceId id = client.voiceBroadcastsApi().create(voiceBroadcast, true);
         assertThat(jsonConverter.serialize(id), equalToIgnoringWhiteSpace(responseJson));
 
         HttpUriRequest arg = captor.getValue();
@@ -72,18 +75,18 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
 
     @Test
     public void testGet() throws Exception {
-        String expectedJson = getJsonPayload(JSON_PATH + "/response/getIvrBroadcast.json");
+        String expectedJson = getJsonPayload(JSON_PATH + "/response/getVoiceBroadcast.json");
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse, expectedJson);
 
-        IvrBroadcast ivrBroadcast = client.ivrBroadcastsApi().get(11L, FIELDS);
-        assertThat(jsonConverter.serialize(ivrBroadcast), equalToIgnoringWhiteSpace(expectedJson));
+        VoiceBroadcast VoiceBroadcast = client.voiceBroadcastsApi().get(11L, FIELDS);
+        assertThat(jsonConverter.serialize(VoiceBroadcast), equalToIgnoringWhiteSpace(expectedJson));
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpGet.METHOD_NAME, arg.getMethod());
         assertNull(extractHttpEntity(arg));
         assertThat(arg.getURI().toString(), containsString(ENCODED_FIELDS));
 
-        client.ivrBroadcastsApi().get(11L);
+        client.voiceBroadcastsApi().get(11L);
         assertEquals(2, captor.getAllValues().size());
         assertThat(captor.getAllValues().get(1).getURI().toString(), not(containsString("fields")));
     }
@@ -92,20 +95,20 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
     public void testGetNullId() throws Exception {
         ex.expectMessage("id cannot be null");
         ex.expect(NullPointerException.class);
-        client.ivrBroadcastsApi().get(null);
+        client.voiceBroadcastsApi().get(null);
     }
 
     @Test
     public void testUpdate() throws Exception {
-        String expectedJson = getJsonPayload(JSON_PATH + "/request/updateIvrBroadcast.json");
+        String expectedJson = getJsonPayload(JSON_PATH + "/request/updateVoiceBroadcast.json");
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse);
 
-        IvrBroadcast ivrBroadcast = new IvrBroadcast();
-        ivrBroadcast.setId(11L);
-        ivrBroadcast.setName("Example API IVR updated");
-        ivrBroadcast.setDialplanXml(
-            "<dialplan name=\"Root\"><record background=\"true\"/><play type=\"tts\">Congratulations! You have successfully configured a CallFire I V R.</play></dialplan>");
-        client.ivrBroadcastsApi().update(ivrBroadcast);
+        VoiceBroadcast voiceBroadcast = new VoiceBroadcast();
+        voiceBroadcast.setId(11L);
+        voiceBroadcast.setName("Example API VB updated");
+        voiceBroadcast.setLiveSoundText("Hello! This is an updated VB config tts");
+        voiceBroadcast.setAnsweringMachineConfig(AnsweringMachineConfig.LIVE_IMMEDIATE);
+        client.voiceBroadcastsApi().update(voiceBroadcast);
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpPut.METHOD_NAME, arg.getMethod());
@@ -117,8 +120,8 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
     public void testUpdateNullId() throws Exception {
         ex.expectMessage("id cannot be null");
         ex.expect(NullPointerException.class);
-        IvrBroadcast ivrBroadcast = new IvrBroadcast();
-        client.ivrBroadcastsApi().update(ivrBroadcast);
+        VoiceBroadcast VoiceBroadcast = new VoiceBroadcast();
+        client.voiceBroadcastsApi().update(VoiceBroadcast);
     }
 
     @Test
@@ -131,7 +134,7 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
             .fields(FIELDS)
             .id(11L)
             .build();
-        Page<Batch> batches = client.ivrBroadcastsApi().getBatches(request);
+        Page<Batch> batches = client.voiceBroadcastsApi().getBatches(request);
         assertThat(jsonConverter.serialize(batches), equalToIgnoringWhiteSpace(expectedJson));
 
         HttpUriRequest arg = captor.getValue();
@@ -148,15 +151,16 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
         String requestJson = getJsonPayload(JSON_PATH + "/request/addBatch.json");
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse, responseJson);
 
-        Recipient r = new Recipient();
-        r.setPhoneNumber("12135551100");
+        Recipient r1 = new Recipient();
+        r1.setPhoneNumber("12135551100");
+        Recipient r2 = new Recipient();
+        r2.setPhoneNumber("12135551101");
         AddBatchRequest request = AddBatchRequest.create()
             .campaignId(15L)
-            .name("contact batch name1")
-            .recipients(Collections.singletonList(r))
-            .scrubDuplicates(true)
+            .name("batch name")
+            .recipients(asList(r1, r2))
             .build();
-        ResourceId id = client.ivrBroadcastsApi().addBatch(request);
+        ResourceId id = client.voiceBroadcastsApi().addBatch(request);
         assertThat(jsonConverter.serialize(id), equalToIgnoringWhiteSpace(responseJson));
 
         HttpUriRequest arg = captor.getValue();
@@ -175,7 +179,7 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
             .fields(FIELDS)
             .id(11L)
             .build();
-        Page<Call> calls = client.ivrBroadcastsApi().getCalls(request);
+        Page<Call> calls = client.voiceBroadcastsApi().getCalls(request);
         assertThat(jsonConverter.serialize(calls), equalToIgnoringWhiteSpace(expectedJson));
 
         HttpUriRequest arg = captor.getValue();
@@ -192,8 +196,8 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
 
         ex.expectMessage("id cannot be null");
         ex.expect(NullPointerException.class);
-        client.ivrBroadcastsApi().start(null);
-        client.ivrBroadcastsApi().start(10L);
+        client.voiceBroadcastsApi().start(null);
+        client.voiceBroadcastsApi().start(10L);
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpPost.METHOD_NAME, arg.getMethod());
@@ -206,8 +210,8 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
 
         ex.expectMessage("id cannot be null");
         ex.expect(NullPointerException.class);
-        client.ivrBroadcastsApi().stop(null);
-        client.ivrBroadcastsApi().stop(10L);
+        client.voiceBroadcastsApi().stop(null);
+        client.voiceBroadcastsApi().stop(10L);
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpPost.METHOD_NAME, arg.getMethod());
@@ -220,8 +224,8 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
 
         ex.expectMessage("id cannot be null");
         ex.expect(NullPointerException.class);
-        client.ivrBroadcastsApi().archive(null);
-        client.ivrBroadcastsApi().archive(10L);
+        client.voiceBroadcastsApi().archive(null);
+        client.voiceBroadcastsApi().archive(10L);
 
         HttpUriRequest arg = captor.getValue();
         assertEquals(HttpPost.METHOD_NAME, arg.getMethod());
@@ -235,10 +239,10 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
         ArgumentCaptor<HttpUriRequest> captor = mockHttpResponse(mockHttpClient, mockHttpResponse, responseJson);
 
         Recipient r1 = new Recipient();
-        r1.setPhoneNumber("12135551101");
+        r1.setPhoneNumber("12135551100");
         Recipient r2 = new Recipient();
-        r2.setPhoneNumber("12135551102");
-        List<Call> calls = client.ivrBroadcastsApi().addRecipients(15L, asList(r1, r2));
+        r2.setPhoneNumber("12135551101");
+        List<Call> calls = client.voiceBroadcastsApi().addRecipients(15L, asList(r1, r2));
         assertThat(jsonConverter.serialize(new ListHolder<>(calls)), equalToIgnoringWhiteSpace(responseJson));
 
         HttpUriRequest arg = captor.getValue();
@@ -247,7 +251,7 @@ public class IvrBroadcastsApiTest extends AbstractApiTest {
         assertThat(arg.getURI().toString(), containsString("/15"));
         assertThat(arg.getURI().toString(), not(containsString(ENCODED_FIELDS)));
 
-        client.ivrBroadcastsApi().addRecipients(15L, asList(r1, r2), FIELDS);
+        client.voiceBroadcastsApi().addRecipients(15L, asList(r1, r2), FIELDS);
         assertUriContainsQueryParams(captor.getAllValues().get(1).getURI(), ENCODED_FIELDS);
     }
 }
