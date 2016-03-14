8df8b6d207bb8ffe572a4707f19a267aee077ad2
vladimir-mhl <vladimir.mhl@gmail.com>
2016-03-14 15:32:46 +0200

    fixed mp3 media type

--- src/main/java/com/callfire/api/client/api/callstexts/model/MediaType.java
+++ src/main/java/com/callfire/api/client/api/callstexts/model/MediaType.java
@@ -11,8 +11,8 @@ public enum MediaType {
     BMP("bmp", "image/bmp"),
     GIF("gif", "image/gif"),
     MP4("mp4", "video/mp4"),
-    M4A("m4a", "audio/mp4"),
-    MP3("mp3", "audio/mpeg"),
+    M4A("m4a", "audio/m4a"),
+    MP3("mp3", "audio/mp3"),
     WAV("wav", "audio/x-wav"),
     UNKNOWN("unknown", "application/octet-stream");
 
